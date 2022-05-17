9981116b82ad8572d739f8d050573efebf1c6c3d
YutaK1026 <inheritans1026@gmail.com>
2022-05-17 13:59:49 +0900

    docker_setting

---
+++ backend_Project/backend/setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'main',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'db',
+        'PORT': '3306',
+    }
+}
